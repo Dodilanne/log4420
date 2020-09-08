a544b68a64aa2330de094c719adc0e37e41dc0aa
Vincent Audet <vincent-2.audet@polymtl.ca>
2020-09-08 17:48:39 -0400

    Outlining & HTML validation fixes

--- tp1/js/product-converter.js
+++ tp1/js/product-converter.js
@@ -1,12 +1,13 @@
 const jsonProducts = require("../data/products.json");
 
 const jsonToThumbnail = ({ id, name, price, image }) => {
+  const validName = name.replace('"', ' pouces');
   return `<article id="product-${id}" class="product">
   <a href="./product.html">
     <div class="content-wrapper">
-      <h2>${name}</h2>
-      <img alt="${name}" src="./assets/img/${image}" />
-      <h3>${price}$</h3>
+      <h2>${validName}</h2>
+      <img alt="${validName}" src="./assets/img/${image}" />
+      <h3>Prix courant: ${price}$</h3>
     </div>
   </a>
 </article>`;
