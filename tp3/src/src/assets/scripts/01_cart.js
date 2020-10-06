d6f45151fd508d546e5e1e7ee8638e3bdb8d3ee4
Vincent Audet <vincent-2.audet@polymtl.ca>
2020-10-06 13:54:25 -0400

    Cart script progress

--- tp3/src/src/assets/scripts/01_cart.js
+++ tp3/src/src/assets/scripts/01_cart.js
@@ -1,5 +1,50 @@
-/* global $ */
+/* global $, localStorage */
 
-$(() => {
-    console.log("Hello from the cart.js script! ");
-});
+(() => {
+    const getCart = () => {
+        const cart = JSON.parse(localStorage.getItem("cart"));
+        if (!cart) {
+            localStorage.setItem("cart", JSON.stringify({}));
+        }
+
+        return cart || {};
+    };
+
+    const onCartUpdate = () => {
+        const cart = getCart();
+        const nbOfItems = Object.keys(cart).length;
+        console.log(`Items in cart: ${nbOfItems}`);
+
+        $("header .count").html(nbOfItems.toString());
+    };
+
+    const addItem = (item) => {
+        const cart = {
+            ...getCart(),
+            [item.id]: item,
+        };
+        localStorage.setItem("cart", JSON.stringify(cart));
+    };
+
+    const clearCart = () => {
+        localStorage.setItem("cart", JSON.stringify({}));
+    };
+
+    $(() => {
+        console.log("Hello from the cart.js script! ");
+
+        clearCart();
+
+        onCartUpdate();
+
+        // Test loop
+        let id = 1;
+        const interval = setInterval(() => {
+            if (id === 10) clearInterval(interval);
+
+            addItem({ id, name: `Product ${id}` });
+            id += 1;
+            onCartUpdate();
+        }, 1000);
+    });
+})();
