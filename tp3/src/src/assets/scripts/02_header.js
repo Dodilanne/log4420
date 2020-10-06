5b84ce6409c6d887f82312e9c8d11901352d60a0
PChausseC <pablo.chausse-cossio@polymtl.ca>
2020-10-06 18:05:39 -0400

    test ids and cart contents

---
+++ tp3/src/src/assets/scripts/02_header.js
@@ -0,0 +1,9 @@
+/* global $ */
+(() => {
+    $(() => {
+        cart.addItem({"id": "1","qty": "4"});
+        cart.addItem({"id": "2","qty": "4"});
+        cart.addItem({"id": "3","qty": "4"});
+        
+    });
+})();
