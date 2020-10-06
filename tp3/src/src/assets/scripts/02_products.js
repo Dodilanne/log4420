8b6eee8e107fde7f0904440eaf7e229099bdc737
Vincent Audet <vincent-2.audet@polymtl.ca>
2020-10-06 17:36:37 -0400

    Product criteria filter

--- tp3/src/src/assets/scripts/02_products.js
+++ tp3/src/src/assets/scripts/02_products.js
@@ -1,38 +1,52 @@
 /* global $ */
 
 (() => {
-    const getTemplate = ({ id, name, image, price }) => `<a
-      href="./product.html?id=${id}"
-      title="En savoir plus..."
-      class="col hoverable">
-      <h3>${name}</h3>
-      <img
-      alt="${name}"
-      src="img/${image}"
-      />
-      <p><small>Prix</small> ${price}&thinsp;$</p>
-      </a>`;
+    const getTemplate = ({ id, name, image, price }) => `<div class="product">
+      <a href="./product.html?id=${id}" title="En savoir plus...">
+        <h2>${name}</h2>
+        <img alt="${name}" src="img/${image}">
+        <p class="price"><small>Prix</small> ${price}&thinsp;$</p>
+      </a>
+    </div>`;
+
+    const compareProducts = (a, b) =>
+        typeof a === "string" ? a.localeCompare(b) : a - b;
 
     $(() => {
         if (!$("#products-list").length) return; // Not on products page
 
         let products = [];
 
-        const renderProducts = (sortingKey, sortingOrder) => {
-            const sortedProducts = products.sort((p1, p2) =>
-                sortingOrder === "asc"
-                    ? p1[sortingKey] - p2[sortingKey]
-                    : p2[sortingKey] - p1[sortingKey]
-            );
+        const getSortedProducts = () =>
+            products.sort((p1, p2) => {
+                const selectedFilter = $("#product-criteria button.selected");
+                const sortingKey = selectedFilter.data("sorting-key");
+                const sortingOrder = selectedFilter.data("sorting-order");
+                const isASC = sortingOrder === "asc";
+                const sortingArray = isASC
+                    ? [p1[sortingKey], p2[sortingKey]]
+                    : [p2[sortingKey], p1[sortingKey]];
 
+                return compareProducts(...sortingArray);
+            });
+
+        const updateProducts = () => {
             $("#products-list").html(
-                sortedProducts.map(getTemplate).join("\n")
+                getSortedProducts().map(getTemplate).join("\n")
             );
         };
 
+        $("#product-criteria button").on("click", ({ target }) => {
+            if ($(target).hasClass("selected")) return; // Filter is already selected
+
+            $("#product-criteria button").removeClass("selected");
+            $(target).addClass("selected");
+            updateProducts();
+        });
+
         $.getJSON("data/products.json", (res) => {
             products = res;
-            renderProducts("price", "desc");
+            updateProducts();
         });
     });
 })();
