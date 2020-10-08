f08dac7e8a2a4a0319422e898b98cb9ac30af3fa
PChausseC <pablo.chausse-cossio@polymtl.ca>
2020-10-08 18:02:09 -0400

    shopping cart rows dynamically built

--- tp3/src/src/assets/scripts/02_shopping-cart.js
+++ tp3/src/src/assets/scripts/02_shopping-cart.js
@@ -1,6 +1,37 @@
-/* global $ */
+/* global $, window, cart*/
 
 (() => {
+    const emptyMessage = "<p>Aucun produits dans le panier</p>";
+    const shoppingCartHead = `
+    <tr>
+      <th></th>
+      <th>Produit</th>
+      <th>Prix unitaire</th>
+      <th>Quantité</th>
+      <th>Prix</th>
+    </tr>`;
+    const getProductRow = (id, name, price, qty) => `<tr data-product="${id}">
+  <td><button title="Supprimer" data-id="${id}"><i class="fa fa-times"></i></button></td>
+  <td><a href="./product.html">${name}</a></td>
+  <td>${price}&thinsp;$</td>
+  <td>
+    <div class="row">
+      <div class="col">
+        <button title="Retirer" disabled=""><i class="fa fa-minus"></i></button>
+      </div>
+      <div class="col">${qty}</div>
+      <div class="col">
+        <button title="Ajouter"><i class="fa fa-plus"></i></button>
+      </div>
+    </div>
+  </td>
+  <td>199,99&thinsp;$</td>
+</tr>`;
+    const getBuyRow = (
+        total
+    ) => `<p class="shopping-cart-total">Total: <strong>${total}&thinsp;$</strong></p>
+        <a class="btn pull-right" href="./order.html">Commander <i class="fa fa-angle-double-right"></i></a>
+        <button class="btn" id="emptyCart"><i class="fa fa-trash-o"></i>&nbsp; Vider le panier</button>`;
     const removeProduct = (id) => {
         cart.removeItem(id);
         $(".shopping-cart-table tr")
@@ -11,11 +42,18 @@
     };
 
     $(() => {
-        if(cart.isEmpty) return;
+        if (window.location.pathname !== "/shopping-cart.html") return; // Not on products page
+        if (cart.isEmpty()) {
+            $(".shopping-cart-table").html(emptyMessage);
+            return;
+        }
+        $("thead").html(shoppingCartHead);
+        $("tbody").html(getProductRow("1", "test", "199.99", "4"));
+        $(".shopping-cart-table").after(getBuyRow("199.99"));
         $(".shopping-cart-table button[title|='Supprimer']").on(
             "click",
             function () {
-                var id = $(this).data("id");
+                const id = $(this).data("id");
                 removeProduct(id);
             }
         );
