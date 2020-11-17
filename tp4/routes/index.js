2dfac2300a93908afc6acfcbf15da21aede144a1
Vincent Audet <vincent-2.audet@polymtl.ca>
2020-11-17 14:51:21 -0500

    Router setup

--- tp4/routes/index.js
+++ tp4/routes/index.js
@@ -1,18 +1,11 @@
 const express = require("express");
-const router = express.Router();
-const pages = require("../utils/constants/page-attributes");
 
-Object.keys(pages).forEach((template) => {
-    const { title, paths, additionalScripts } = pages[template];
-    paths.forEach((path) => {
-        router.get(path, (_req, res) => {
-            res.render(`pages/${template}`, {
-                title,
-                additionalScripts,
-                pageName: template,
-            });
-        });
-    });
-});
+const router = express.Router();
+
+const pagesRouter = require("./pages.router");
+const apiRouter = require("./api.router");
+
+router.use("/", pagesRouter);
+router.use("/api", apiRouter);
 
 module.exports = router;
---
+++ tp4/routes/pages.router.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const router = express.Router();
+
+const pages = require("../utils/constants/page-attributes");
+
+Object.keys(pages).forEach((template) => {
+    const { title, paths, additionalScripts } = pages[template];
+    paths.forEach((path) => {
+        router.get(path, (_req, res) => {
+            res.render(`pages/${template}`, {
+                title,
+                additionalScripts,
+                pageName: template,
+            });
+        });
+    });
+});
+
+module.exports = router;
