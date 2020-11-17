bbe39d240ddbdadbe597856a1a4b0829b3cc5b9b
Vincent Audet <vincent-2.audet@polymtl.ca>
2020-11-17 13:17:07 -0500

    Update tab widths

--- tp4/routes/index.js
+++ tp4/routes/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const pages = require('../utils/constants/page-attributes');
+const pages = require("../utils/constants/page-attributes");
 
 Object.keys(pages).forEach((template) => {
-  const { title, paths, additionalScripts } = pages[template];
-  paths.forEach((path) => {
-    router.get(path, (_req, res) => {
-      res.render(`pages/${template}`, { title, additionalScripts });
+    const { title, paths, additionalScripts } = pages[template];
+    paths.forEach((path) => {
+        router.get(path, (_req, res) => {
+            res.render(`pages/${template}`, { title, additionalScripts });
+        });
     });
-  });
 });
 
 module.exports = router;
--- tp4/utils/constants/page-attributes.js
+++ tp4/utils/constants/page-attributes.js
@@ -1,38 +1,38 @@
 module.exports = {
-  index: {
-    title: 'Accueil',
-    paths: ['/', '/accueil'],
-  },
-  products: {
-    title: 'Produits',
-    paths: ['/produits'],
-    additionalScripts: ['/assets/scripts/products.controller.js'],
-  },
-  product: {
-    title: 'Produit',
-    paths: ['/produits/:id'],
-  },
-  contact: {
-    title: 'Contact',
-    paths: ['/contact'],
-  },
-  'shopping-cart': {
-    title: 'Panier',
-    paths: ['/panier'],
-    additionalScripts: [
-      '/assets/js/jquery.validate.min.js',
-      '/assets/js/additional-methods.min.js',
-      '/assets/js/messages_fr.js',
-      '/assets/scripts/orders.service.js',
-      '/assets/scripts/order.controller.js',
-    ],
-  },
-  order: {
-    title: 'Commande',
-    paths: ['/commande'],
-  },
-  confirmation: {
-    title: 'Confirmation',
-    paths: ['/confirmation'],
-  },
+    index: {
+        title: "Accueil",
+        paths: ["/", "/accueil"],
+    },
+    products: {
+        title: "Produits",
+        paths: ["/produits"],
+        additionalScripts: ["/assets/scripts/products.controller.js"],
+    },
+    product: {
+        title: "Produit",
+        paths: ["/produits/:id"],
+    },
+    contact: {
+        title: "Contact",
+        paths: ["/contact"],
+    },
+    "shopping-cart": {
+        title: "Panier",
+        paths: ["/panier"],
+        additionalScripts: [
+            "/assets/js/jquery.validate.min.js",
+            "/assets/js/additional-methods.min.js",
+            "/assets/js/messages_fr.js",
+            "/assets/scripts/orders.service.js",
+            "/assets/scripts/order.controller.js",
+        ],
+    },
+    order: {
+        title: "Commande",
+        paths: ["/commande"],
+    },
+    confirmation: {
+        title: "Confirmation",
+        paths: ["/confirmation"],
+    },
 };
