14e752f36731a45a3fd356c0847dfb9506fcb08f
PChausseC <pablo.chausse-cossio@polymtl.ca>
2020-11-18 16:36:12 -0500

    shopping cart api

--- tp4/routes/api.router.js
+++ tp4/routes/api.router.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 const products = require("./products.router");
 
+const shopping_cart = require("./shopping-cart.router");
+
 router.use("/products", products);
+router.use("/shopping-cart", shopping_cart);
 
 module.exports = router;
