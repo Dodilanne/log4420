665a7b0af2d880faba143dda24537d12cb19a16e
PChausseC <pablo.chausse-cossio@polymtl.ca>
2020-11-18 20:47:49 -0500

    shopping cart get, get by id and post

--- tp4/routes/shopping-cart.router.js
+++ tp4/routes/shopping-cart.router.js
@@ -2,14 +2,36 @@ const express = require("express");
 
 const router = express.Router();
 
-const ordersController = require("../controllers/orders.controller");
-
 router.get("/", async (req, res, next) => {
     try {
-       
-       res.json(
-        await ordersController.get({orderID : req.session.orderID})
-       );
+        res.json(req.session.cart);
+    } catch (e) {
+        console.log(e.message);
+        next(e);
+    }
+});
+
+router.get("/:productID", async (req, res, next) => {
+    try {
+        if (req.session.cart) {
+            let product = req.session.cart.find(
+                (item) => item.productID == req.params.productID
+            );
+            if (product) res.json(product);
+        }
+        res.sendStatus(404);
+    } catch (e) {
+        console.log(e.message);
+        next(e);
+    }
+});
+
+router.post("/", async (req, res, next) => {
+    try {
+        let session = req.session;
+        if (!session.cart) session.cart = [];
+        session.cart.push(req.body);
+        res.json(req.session.cart);
     } catch (e) {
         console.log(e.message);
         next(e);
