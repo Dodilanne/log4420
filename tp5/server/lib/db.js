fa7220ff1aa1cf77bea3855ad9db0345e4b09aa2
Vincent Audet <vincent-2.audet@polymtl.ca>
2020-11-24 16:05:13 -0500

    Fix mongoose version problem, Add appletv img to image loader

--- tp5/server/lib/db.js
+++ tp5/server/lib/db.js
@@ -3,24 +3,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Order = new Schema({
-  id: { type: Number, unique: true },
-  firstName: String,
-  lastName: String,
-  email: String,
-  phone: String,
-  products: Array
-}, { versionKey: false });
+const Order = new Schema(
+  {
+    id: { type: Number, unique: true },
+    firstName: String,
+    lastName: String,
+    email: String,
+    phone: String,
+    products: Array,
+  },
+  { versionKey: false }
+);
 
-const Product = new Schema({
-  id: { type: Number, unique: true },
-  name: String,
-  price: Number,
-  image: String,
-  category: String,
-  description: String,
-  features: Array
-}, { versionKey: false });
+const Product = new Schema(
+  {
+    id: { type: Number, unique: true },
+    name: String,
+    price: Number,
+    image: String,
+    category: String,
+    description: String,
+    features: Array,
+  },
+  { versionKey: false }
+);
 
 mongoose.model("Order", Order);
 mongoose.model("Product", Product);
@@ -28,4 +34,7 @@ mongoose.model("Product", Product);
 mongoose.Promise = global.Promise;
 
 // TODO: Modifier le connect string par le votre!
-mongoose.connect("mongodb://...", { useMongoClient: true });
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0.ntgsa.mongodb.net/online_shop?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
