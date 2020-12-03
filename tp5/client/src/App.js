5818f3804d70dd2f41838b2bf81636506747fb9c
Vincent Audet <vincaudet@gmail.com>
2020-12-03 10:15:45 -0500

    Implementation description

--- tp5/client/src/App.js
+++ tp5/client/src/App.js
@@ -6,6 +6,17 @@ import Router from './foundation/Router/Router';
 
 configureAxios();
 
+/*
+* README @chargé :
+* Our implementation relies on the use a Redux store, which allows
+* us to keep an application level state that persists throught navigation.
+* After an initial fetch of the data (see the Router component), components then
+* obtain their data directly from the Redux store throught the use of the useSelector
+* hook. This removes the need of constantly fetching data from the server and passing 
+* heavy objects throught component props. The store state is kept up to date by dispatching
+* a corresponding Redux action whenever a state modifying request is successfully processed
+* by the server (See client/foundation/thunks).
+**/
 const { store, persistor } = createStore();
 
 function App() {
