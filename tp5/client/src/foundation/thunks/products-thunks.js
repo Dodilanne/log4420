563dcdc25dd924218fcdc5d6c38b1379c8402e58
Vincent Audet <vincaudet@gmail.com>
2020-12-03 10:15:57 -0500

    Thunks cleanup

--- tp5/client/src/foundation/thunks/products-thunks.js
+++ tp5/client/src/foundation/thunks/products-thunks.js
@@ -2,12 +2,6 @@ import axios from 'axios';
 import { tryCatchWrapper } from '../../utils';
 import { productsActions } from '../store/products-reducer';
 
-export const fetchProduct = async productID =>
-  tryCatchWrapper(async () => {
-    const res = await axios.get(`/products/${productID}`);
-    return res.data;
-  });
-
 export const fetchProducts = () => async dispatch =>
   tryCatchWrapper(async () => {
     const res = await axios.get(`/products`);
--- tp5/client/src/foundation/thunks/shopping-cart-thunks.js
+++ tp5/client/src/foundation/thunks/shopping-cart-thunks.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { deepParseInt, tryCatchWrapper } from '../../utils';
+import { arrayFindByKey, deepParseInt, tryCatchWrapper } from '../../utils';
 import { shoppingCartActions } from '../store/shopping-cart-reducer';
 
 export const fetchCart = () => async dispatch =>
@@ -8,47 +8,41 @@ export const fetchCart = () => async dispatch =>
     dispatch(shoppingCartActions.populate(res.data));
   });
 
-export const updateProductQuantity = ({
-  productId,
-  quantity,
-}) => async dispatch =>
+export const updateProductQuantity = data => async dispatch =>
   tryCatchWrapper(async () => {
+    const payload = deepParseInt(data);
+    const { productId, quantity } = payload;
     await axios.put(`/shopping-cart/${productId}`, { quantity });
-    dispatch(shoppingCartActions.updateItem({ productId, quantity }));
-    return true;
+    dispatch(shoppingCartActions.updateItem(payload));
   });
 
 export const addProductToCart = data => async (dispatch, getState) =>
   tryCatchWrapper(async () => {
     const payload = deepParseInt(data);
     const { shoppingCart } = getState();
-    const itemFound = shoppingCart.find(
-      ({ productId }) => productId === payload.productId
-    );
-    if (!itemFound) {
-      await axios.post('/shopping-cart', payload);
-      dispatch(shoppingCartActions.addItem(payload));
-      return true;
+
+    const itemFound = arrayFindByKey(shoppingCart, payload.productId);
+    if (itemFound) {
+      return dispatch(
+        updateProductQuantity({
+          ...payload,
+          quantity: itemFound.quantity + payload.quantity,
+        })
+      );
     }
 
-    return dispatch(
-      updateProductQuantity({
-        ...payload,
-        quantity: itemFound.quantity + payload.quantity,
-      })
-    );
+    await axios.post('/shopping-cart', payload);
+    dispatch(shoppingCartActions.addItem(payload));
   });
 
 export const removeProductFromCart = productId => async dispatch =>
   tryCatchWrapper(async () => {
     await axios.delete(`/shopping-cart/${productId}`);
     dispatch(shoppingCartActions.removeItem(productId));
-    return true;
   });
 
 export const clearCart = () => async dispatch =>
   tryCatchWrapper(async () => {
     await axios.delete('/shopping-cart');
     dispatch(shoppingCartActions.clear());
-    return true;
   });
